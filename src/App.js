bd0031d7df09046e777757ed30044a6d60ab0530
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2022-08-24 11:49:32 +0530

    q, start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  const onStartClick = ()=>{
+    console.log("onstartclick");
+    setStart(true)
+  }
+  const onClick= () =>{
+    console.log("inside onClick submit")
+    if(ua == a){
+      setm('Right');
+    }else{
+      setm('Wrong');
+    }
+  }
+  var [isStarted, setStart] = useState(false);
+  var [a, seta] = useState('20');
+  var [ua, setua] = useState('');
+  var [m, setm] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isStarted && <button onClick={onStartClick}>Start</button>}
+      {isStarted && 
+        <div class="q">
+          <h1>Q</h1>
+          <h2>10+10?</h2>
+          <h3>{m}</h3>
+          <input type="number" name="a" id="a" 
+            value={ua}
+            onChange={e=> setua(e.target.value)}
+          />
+          <button onClick={onClick}>Submit</button> 
+        </div>
+      }
     </div>
   );
 }
