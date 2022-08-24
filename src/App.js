51fe8c87027d7679ef4d7b17f9041c2a24d02027
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2022-08-24 11:56:20 +0530

    try again

--- src/App.js
+++ src/App.js
@@ -13,24 +13,43 @@ function App() {
     }else{
       setm('Wrong');
     }
+    setUserAnswered(true);
+  }
+  const onTryAgain = () =>{
+    console.log("ontryagain");
+    resetTest()
+  }
+  const resetTest =() =>{
+    console.log("reset test");
+    seta(false);
+    setua('');
+    setm('');
+    setUserAnswered(false)
   }
   var [isStarted, setStart] = useState(false);
   var [a, seta] = useState('20');
   var [ua, setua] = useState('');
   var [m, setm] = useState('');
+  var [isUserAnswered, setUserAnswered] = useState(false);
   return (
     <div className="App">
       {!isStarted && <button onClick={onStartClick}>Start</button>}
       {isStarted && 
-        <div class="q">
+        <div className="q">
           <h1>Q</h1>
           <h2>10+10?</h2>
           <h3>{m}</h3>
-          <input type="number" name="a" id="a" 
-            value={ua}
-            onChange={e=> setua(e.target.value)}
-          />
-          <button onClick={onClick}>Submit</button> 
+
+          {!isUserAnswered &&
+            <div className="ua">
+              <input type="number" name="a" id="a" 
+                value={ua}
+                onChange={e=> setua(e.target.value)}
+              />
+              <button onClick={onClick}>Submit</button> 
+            </div>
+          }
+          { isUserAnswered && <button onClick={onTryAgain}>Try Again</button>}
         </div>
       }
     </div>
