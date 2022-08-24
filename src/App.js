d4d6cb3419b67137849100ee6eb911fec7016779
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2022-08-24 12:26:11 +0530

    10s timeout

--- src/App.js
+++ src/App.js
@@ -8,12 +8,41 @@ function App() {
     document.head.innerHTML+=`
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-    `
+    `;
+
+   
   }, []);
+
+  const updateTimeLeft = () => {
+    console.log("inside update time left");
+    setTimeLeft(t => {
+      t-= 1;
+      console.log("tl:", t, typeof(t))
+      if(t <=0){
+        // debugger;
+        console.log("time out");
+        setm('Timeout');
+        setua('timeout');
+        setUserAnswered(true);
+  
+        // clear Interval
+        setIntervalObj(iob => {
+          //clear interval
+          clearInterval(iob);
+          return iob;
+        })
+        return 0;
+      }
+      return t;
+    });
+   
+  }
 
   const onStartClick = ()=>{
     console.log("onstartclick");
-    setStart(true)
+    setStart(true);
+    var iobj = setInterval(updateTimeLeft, 1000);// 1/10 second
+    setIntervalObj(iobj);
   }
   const onClick= () =>{
     console.log("inside onClick submit")
@@ -40,6 +69,8 @@ function App() {
   var [ua, setua] = useState('');
   var [m, setm] = useState('');
   var [isUserAnswered, setUserAnswered] = useState(false);
+  var [timeLeft, setTimeLeft] = useState(10);// 20 seconds
+  var [intervalObj, setIntervalObj] = useState(null);
   return (
     <div className="App">
       {!isStarted && <button onClick={onStartClick}>Start</button>}
@@ -56,7 +87,8 @@ function App() {
                 onChange={e=> setua(e.target.value)}
               />
               <br/>
-              <button onClick={onClick}>Submit</button> 
+              <button onClick={onClick}>{timeLeft>0 ?timeLeft : ''} Submit</button> 
+
             </div>
           }
           { isUserAnswered && <button onClick={onTryAgain}>Try Again</button>}
